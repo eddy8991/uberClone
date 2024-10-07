4cb6fb3b0651819f58ef257f15c5b5314ddb06cc
Edwin_K <edumwas911@gmail.com>
2024-10-07 10:23:32 +0300

    build custom button

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,10 @@
 /* eslint-disable prettier/prettier */
-import { View, Text } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
+import { Redirect } from "expo-router";
 
 
 const Home = () =>{
-    return (
-        <SafeAreaView>
-            <Text>Home</Text>
-        </SafeAreaView>
-    )
+    return <Redirect href="./(auth)/welcome"/>
+    //check hoe redirect works
 }
+
+export default Home;
